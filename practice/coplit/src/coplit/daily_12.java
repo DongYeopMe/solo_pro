70f134bc10e76db19b294a98b53ca983e25fe465
Dongyeop Lee <dlduq29@gmail.com>
2023-01-29 02:01:32 +0900

    Daily coding 코플릿 12번 문제

---
+++ practice/coplit/src/coplit/daily_12.java
@@ -0,0 +1,20 @@
+package coplit;
+import java.util.*;
+public class daily_12 {
+    public static void main(String[] args) {
+        int[] output = reverseArr([1, 2, 3]);
+        System.out.println(Arrays.toString(output)); // --> [3, 2, 1]
+    }
+    public static int[] reverseArr(int[] arr){
+        // TODO:
+        if(arr.length==0)return new int[]{};
+        int[] head = Arrays.copyOfRange(arr,arr.length-1,arr.length);
+        int[] tail = reverseArr(Arrays.copyOfRange(arr,0,arr.length-1));
+
+        int[] dust = new int[head.length+tail.length];
+        System.arraycopy(head,0,dust,0,head.length);
+        System.arraycopy(tail,0,dust,head.length,tail.length);
+        return dust;
+    }
+
+}
