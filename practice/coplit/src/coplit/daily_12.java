af16c576bd3ad65ec9d374fb8b3cbbb63c239be3
Dongyeop Lee <dlduq29@gmail.com>
2023-01-29 02:02:30 +0900

    Daily coding 코플릿 12번 문제

--- practice/coplit/src/coplit/daily_12.java
+++ practice/coplit/src/coplit/daily_12.java
@@ -2,7 +2,7 @@ package coplit;
 import java.util.*;
 public class daily_12 {
     public static void main(String[] args) {
-        int[] output = reverseArr([1, 2, 3]);
+        int[] output = reverseArr(new int[]{1,2,3});
         System.out.println(Arrays.toString(output)); // --> [3, 2, 1]
     }
     public static int[] reverseArr(int[] arr){
