e76e0dd83055d30977beda18cf058cc7953a211a
Dongyeop Lee <dlduq29@gmail.com>
2023-04-09 23:54:58 +0900

    first commit

---
+++ spring_project/To_Do_App/src/main/java/com/project/To_Do_App/ToDoAppApplication.java
@@ -0,0 +1,18 @@
+package com.project.To_Do_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class ToDoAppApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoAppApplication.class, args);
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(ToDoAppApplication.class);
+	}
+}
