1ca9ae3eb47af88988ceaaa70dcc3cdc53f4c9cb
Dongyeop Lee <70151275+DongYeopMe@users.noreply.github.com>
2023-04-10 22:47:49 +0900

    Delete practice/sample/src/main directory

--- practice/sample/src/main/java/com/example/sample/book/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.sample.book;
-
-import com.example.sample.book.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-
-}
